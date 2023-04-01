db597042c1dcedacd428cb53fdae11137e1ba7ff
dorfollyud <rodolfo.dorfo@outlook.com>
2023-03-31 21:40:33 -0300

    iniciando o sistema de re dos IA

--- Assets/Standard Assets/Vehicles/Car/Scripts/CarAIControl.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarAIControl.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -45,6 +46,8 @@ namespace UnityStandardAssets.Vehicles.Car
         private float m_AvoidOtherCarSlowdown;    // how much to slow down due to colliding with another car, whilst avoiding
         private float m_AvoidPathOffset;          // direction (-1 or 1) in which to offset path to avoid other car, whilst avoiding
         private Rigidbody m_Rigidbody;
+
+        private bool BarrierStop = false;
 
 
         private void Awake()
@@ -170,18 +173,35 @@ namespace UnityStandardAssets.Vehicles.Car
                         float steer = Mathf.Clamp(targetAngle*m_SteerSensitivity, -1, 1)*Mathf.Sign(m_CarController.CurrentSpeed);
 
                         // feed input to the car controller.
-                        m_CarController.Move(steer, accel, accel, 0f);
-
+                        if(BarrierStop == false){
+                            m_CarController.Move(steer, accel, accel, 0f);
+                        }
+                        if(BarrierStop == true){
+                            m_CarController.Move(steer, -accel, -accel, 0f);
+                        }
+                       
                         // if appropriate, stop driving when we're close enough to the target.
                         if (m_StopWhenTargetReached && localTarget.magnitude < m_ReachTargetThreshold)
                         {
                             m_Driving = false;
                         }
-                    }
+                    
                 }
+            }
                 
         }
 
+        private void OnTriggerEnter(Collider other){
+            if(other.gameObject.CompareTag("Barrier")){
+                BarrierStop = true;
+            }
+        }
+
+        private void OnTriggerExit(Collider other){
+            if(other.gameObject.CompareTag("Barrier")){
+                StartCoroutine(BarrierReset());
+            }
+        }
 
         private void OnCollisionStay(Collision col)
         {
@@ -226,6 +246,11 @@ namespace UnityStandardAssets.Vehicles.Car
         {
             m_Target = target;
             m_Driving = true;
+        }
+
+        IEnumerator BarrierReset(){
+            yield return new WaitForSeconds(1.5f);
+            BarrierStop = false;
         }
     }
 }
